242e0e6dc4697366737b1709ec325079a1b6217b
kim-sardine <kim.sardine@gmail.com>
2020-07-25 01:16:58 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Random Picker
+
+Side-punch project
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      Hello random picker!
+    </div>
+  );
+}
+
+export default App;
