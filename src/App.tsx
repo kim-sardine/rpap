ff02da1894b1194f4bbfcdb5f0feeab0da29f36d
kim-sardine <kim.sardine@gmail.com>
2020-07-26 01:22:23 +0900

    Add basic implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
+
+import './App.css';
 
 function App() {
+  const [data, setData] = useState('');
+  const handle = useFullScreenHandle();
+
+  const onChangeInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setData(e.target.value);
+  };
+
+  const onSubmitData = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    // Generate FullScreen presenter from data
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      Hello random picker!
+    <div>
+      <div>
+        <textarea value={data} onChange={onChangeInput} />
+        <button onClick={onSubmitData}>Run</button>
+      </div>
+
+      <div>
+        <button onClick={handle.enter}>
+          Enter fullscreen
+        </button>
+        <FullScreen handle={handle}>
+          <div className="title">
+            {data}
+          </div>
+        </FullScreen>
+      </div>
     </div>
   );
 }
