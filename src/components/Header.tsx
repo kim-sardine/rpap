99a7973d457f0a4293e610e82ac2b440d17d6ac0
kim-sardine <kim.sardine@gmail.com>
2020-09-01 01:59:04 +0900

    Add how to use modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Button, Typography } from '@material-ui/core';
+import {
+    AppBar,
+    Toolbar,
+    Button,
+    Typography,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle
+} from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
     appBar: {
@@ -17,15 +27,51 @@ const useStyles = makeStyles((theme) => ({
 
 function Header() {
     const classes = useStyles();
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
 
     return (
         <AppBar position="static" className={classes.appBar}>
             <Toolbar className={classes.toolBar}>
-                <Button color="inherit" className={classes.noTransform}>How to use</Button>
+                <Button color="inherit" className={classes.noTransform} onClick={handleClickOpen}>How to use</Button>
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">{"How to use RPAP"}</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            <img src="/assets/how_to_use.gif" alt="how to use" width="100%" />
+                            <Typography gutterBottom>
+                                1. Copy data from Excel <br/>
+                                2. Paste it in the input box <br/>
+                                3. Enter a title <br/>
+                                4. Click "SHUFFLE & RUN" button <br/>
+                                5. Click the "LET'S START" button on the right <br/>
+                                (You can click "Fullscreen" button on the top right first) <br/>
+                                6. Click the "Next" button to see the next winner. <br/>
+                            </Typography>
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose} color="primary">
+                            Close
+                        </Button>
+                    </DialogActions>
+                </Dialog>
                 <Typography variant="h6">
                     RPAP - Random Pick and Presenter
                 </Typography>
-                <Button color="inherit" className={classes.noTransform} target="_blank" href="https://sidepun.ch">Side-Punch</Button>
+                <Button color="inherit" className={classes.noTransform} target="_blank" href="https://sidepun.ch">sidepun.ch</Button>
             </Toolbar>
         </AppBar>
     );
